a91b6c2df74fb160c89639fcd1fa4098a23cc6fc
parasmehan123 <parasmehan123@gmail.com>
2019-10-30 00:09:37 +0530

    commit

--- Main.java
+++ Main.java
@@ -1,27 +1,23 @@
 package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Scene sc=new Scene(new VBox(new Label("A JavaFX Label")));
-        sc.setCursor(Cursor.OPEN_HAND);
-        primaryStage.setScene(sc);
-        primaryStage.show();
 
+        Parent root=FXMLLoader.load(getClass().getResource("try1.fxml"));
+        primaryStage.setTitle("PlantsVsZombies");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 
 }
