e48596e7a61a926fb31b6db58c1a5501881ab310
Pawan Mehan <parasmehan123@gmail.com>
2019-11-29 16:11:32 +0530

    zombies move started

--- Character.java
+++ Character.java
@@ -14,4 +14,27 @@ public abstract class Character implements Serializable {
         this.health = health;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
 }
--- Zombie.java
+++ Zombie.java
@@ -1,10 +1,28 @@
 package sample;
 
-public abstract class Zombie extends Character{
+import javafx.scene.image.Image;
 
-    protected Zombie(int x,int y,int health)
+import java.awt.*;
+
+public class Zombie extends Character{
+
+    public static Image normal_image,conehead_image;
+
+    static
+    {
+        String path=Main2.path;
+        normal_image=Helper.getImage(path+"TheAdvancing_zombie.gif");
+        conehead_image=Helper.getImage(path+"Conehead_Zombie.gif");;
+    }
+
+    private final int attack_strength;
+    protected Zombie(int x,int y,int health,int attack_strength)
     {
         super(x,y,health);
+        this.attack_strength=attack_strength;
     }
+
+    //TODO
+    public void die(){}
 
 }
